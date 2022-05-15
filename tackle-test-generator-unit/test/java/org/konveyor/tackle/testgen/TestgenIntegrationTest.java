0530b10c9a852a9d6c615b9762b548802b0c5668
Saurabh Sinha <sinha108@gmail.com>
2022-05-15 16:13:44 -0400

    Increased time limit for tests

--- tackle-test-generator-unit/test/java/org/konveyor/tackle/testgen/TestgenIntegrationTest.java
+++ tackle-test-generator-unit/test/java/org/konveyor/tackle/testgen/TestgenIntegrationTest.java
@@ -118,7 +118,7 @@ public class TestgenIntegrationTest {
             // create building-block test sequences using combined test generator
             TestSequenceInitializer seqInitializer = new TestSequenceInitializer(testApp.appName,
                 testApp.testPlanFilename, testApp.appPath, testApp.appClasspathFilename,
-                Constants.COMBINED_TEST_GENERATOR_NAME, 5, false, false, "");
+                Constants.COMBINED_TEST_GENERATOR_NAME, 7, false, false, "");
             seqInitializer.createInitialTests();
 
             // assert that test sequences files are created
